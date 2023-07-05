0f70bf6b5fa0505dc42a5200f81deebe725081b7
Ruben Nobre <ist199321@tecnico.ulisboa.pt>
2023-07-05 16:13:21 +0100

    fixed forEach squiggles

--- src/requests.ts
+++ src/requests.ts
@@ -4,7 +4,7 @@ async function getQuestions() {
     let questionArray: Array<Question> = []
     const response = await fetch('https://the-trivia-api.com/v2/questions?difficulties=easy&limit=50')
         .then(response => response.json())
-        .then(data => data.forEach(question => questionArray.push(new Question(question))))
+        .then(data => data.forEach((question: Question) => questionArray.push(new Question(question))))
     return await questionArray
 }
 
